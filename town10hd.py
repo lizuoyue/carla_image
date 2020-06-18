ca10a4bda6bba13437b59a82767dad028af7f56c
Zuoyue Li <li.zuoyue@foxmail.com>
2020-06-18 15:18:40 +0200

    a

--- town10hd.py
+++ town10hd.py
@@ -32,11 +32,9 @@ if __name__ == '__main__':
     end = beg + num_frames
     gap = 2.0
     stand_at = 3.0
-    rgb_dict = {}
 
     client = carla.Client('localhost', 2000)
     client.set_timeout(10.0)
-    # print(client.get_available_maps())
 
     world = client.get_world()
     world = client.load_world('Town10HD')
@@ -46,19 +44,24 @@ if __name__ == '__main__':
     settings.fixed_delta_seconds = 1.0
     world.apply_settings(settings)
 
+    cam_bp = {
+        'rgb': world.get_blueprint_library().find('sensor.camera.rgb'),
+        'dep': world.get_blueprint_library().find('sensor.camera.depth'),
+        'sem': world.get_blueprint_library().find('sensor.camera.semantic_segmentation'),
+    }
+    for _, bp in cam_bp.items():
+        bp.set_attribute('image_size_x', '2048')
+        bp.set_attribute('image_size_y', '2048')
+        bp.set_attribute('fov', '90')
+        bp.set_attribute('sensor_tick', '0.0')
+
+    cam_dict = {}
+    cam = {k: [] for k in cam_bp}
+    res = {k: [] for k in cam_bp}
+    callback = lambda li, cam_id: lambda image: li.append((cam_id, image))
+
     spawn_points = world.get_map().get_spawn_points()
-    rgb_cam_bp = world.get_blueprint_library().find('sensor.camera.rgb')
-    rgb_cam_bp.set_attribute('image_size_x', '2048')
-    rgb_cam_bp.set_attribute('image_size_y', '2048')
-    rgb_cam_bp.set_attribute('fov', '90')
-    rgb_cam_bp.set_attribute('sensor_tick', '0.0')
-
-    images = []
-    func = lambda cam_id: lambda image: images.append((cam_id, image))
-
-    rgb_cams = []
     for spa_idx, spawn_point in enumerate(spawn_points):
-
         if spa_idx != 5:
             continue
 
@@ -76,23 +79,28 @@ if __name__ == '__main__':
         for loc_idx, cam_loc in enumerate(cam_locs):
             for dir_idx, cam_rot in zip('f,u,d,r,b,l'.split(','), cam_rots):
                 cam_state = carla.Transform(cam_loc, cam_rot)
-                rgb_cam = world.spawn_actor(rgb_cam_bp, cam_state)
-                rgb_cams.append(rgb_cam)
-                rgb_dict[rgb_cam.id] = (spa_idx, loc_idx, dir_idx)
+                for cam_type, bp in cam_bp.items():
+                    cam_ins = world.spawn_actor(bp, cam_state)
+                    cam[cam_type].append(cam_ins)
+                    cam_dict[cam_ins.id] = (spa_idx, loc_idx, dir_idx)
 
-    print(f'Totally {len(rgb_cams)} cameras.')
+    print('Totally %dx3 cameras.' % len(cam['rgb']))
 
-    for rgb_cam in rgb_cams:
-        rgb_cam.listen(func(rgb_cam.id))
+    for cam_type in cam_bp:
+        for cam_ins in cam[cam_type]:
+            cam_ins.listen(callback(res[cam_type], cam_ins.id))
 
     num = world.tick()
     print(f'Getting images from frame {num} ...')
-    while len(images) < len(rgb_cams):
+    while sum([len(res[cam_type]) < len(cam[cam_type]) for cam_type in cam_bp]) > 0:
         continue
     print(f'Done!')
 
-    for cam_id, image in images:
-        image.save_to_disk('pinhole/%02d_%02d_%s.png' % rgb_dict[cam_id])
+    for cam_type in cam_bp:
+        for cam_id, image in res[cam_type]:
+            print(cam_type, cam_id)
+            image.save_to_disk(f'pinhole_{cam_type}/%02d_%02d_%s.png' % cam_dict[cam_id])
 
-    for rgb_cam in rgb_cams:
-        rgb_cam.destroy()
+    for cam_type in cam_bp:
+        for cam_ins in cam[cam_type]:
+            cam_ins.destroy()
